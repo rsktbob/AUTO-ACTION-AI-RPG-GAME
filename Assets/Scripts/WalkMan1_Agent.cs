43b53b0ea36bff5b2fe1d4798456800087bca2d2
thaksin5 <128176822+GaoWeiChang@users.noreply.github.com>
2024-02-28 00:21:34 +0800

    set walkman_agent stucture

--- Assets/Scripts/WalkMan1_Agent.cs
+++ Assets/Scripts/WalkMan1_Agent.cs
@@ -6,7 +6,43 @@ using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 using System;
 
+
 public class WalkMan1_Agent : Agent
 {
+    [SerializeField] private GameObject robotPrefab;
+    [SerializeField] private float InitPositionMinY;
+    [SerializeField] private float InitPositionMaxY;
+    [SerializeField] private float rewardUpperLimit;
+    private Walkman walkman;
 
+    // reset the agent or remove it from the environment
+    public override void OnEpisodeBegin()
+    {
+    }
+
+    // to specify agent behavior at every step, based on the provided action
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+    }
+
+    // to collect the vector observations of the agent for the step.
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+
+    public void Heuristic(float[] actionsOut)
+    {
+
+    }
+
+    /*
+    private Vector3 RelativeBodyPosition(Vector3 position)
+    {
+
+    }
+    */
+
+    private void AddRewardToRobot() { 
+    }
 }
